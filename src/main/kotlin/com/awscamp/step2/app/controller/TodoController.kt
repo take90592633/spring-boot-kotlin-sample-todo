749a7921dcd6ddad2ae71f6f93cf49b78c6870a1
take90592633 <107292079+take90592633@users.noreply.github.com>
2022-10-26 22:41:53 +0900

    Fix layout

--- src/main/kotlin/com/awscamp/step2/app/controller/TodoController.kt
+++ src/main/kotlin/com/awscamp/step2/app/controller/TodoController.kt
@@ -4,7 +4,6 @@ import com.awscamp.step2.app.service.TodoService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam

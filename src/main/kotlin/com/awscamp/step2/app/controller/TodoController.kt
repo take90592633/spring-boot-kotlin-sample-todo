0341bb3b162693d563b9547edcf5667a7649a126
take90592633 <107292079+take90592633@users.noreply.github.com>
2022-10-28 23:06:02 +0900

    Add aws ses service

--- src/main/kotlin/com/awscamp/step2/app/controller/TodoController.kt
+++ src/main/kotlin/com/awscamp/step2/app/controller/TodoController.kt
@@ -1,6 +1,8 @@
 package com.awscamp.step2.app.controller
 
+import com.awscamp.step2.app.service.AwsSesService
 import com.awscamp.step2.app.service.TodoService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,12 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam
 @Controller
 @RequestMapping("/todo")
 class TodoController(
-    private val todoService: TodoService
+    private val todoService: TodoService,
+    private val awsSesService: AwsSesService
 ) {
+    @Value("\${aws.s3.bucket}")
+    private val awsS3Bucket: String = ""
+
     @GetMapping("/list")
     fun todo(
         model: Model
     ): String {
+        awsSesService.sendSes()
         return todoList(model = model)
     }
 
